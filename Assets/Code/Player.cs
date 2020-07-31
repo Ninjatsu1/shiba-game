99f53f3c3ab27eae006e106e85adab439a18ee0d
Nina Mäkinen <sherlocked1223@gmail.com>
2020-07-31 12:15:46 +0300

    Assets imported. Cinemamachine imported. Camera follows player.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -31,7 +31,7 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         //Kutsutaan funktiot
